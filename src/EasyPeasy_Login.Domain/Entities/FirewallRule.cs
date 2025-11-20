3c1f79431e0b5283e93bac149b6b6a9e3d10702d
Rlianny <revelianny10@gmail.com>
2025-11-20 10:03:10 -0500

    chore: updated folder structure

--- src/EasyPeasy_Login.Domain/Entities/FirewallRule.cs
+++ src/EasyPeasy_Login.Domain/Entities/FirewallRule.cs
@@ -2,7 +2,6 @@ class FirewallRule
 {
     public string IPAdress { get; set; }
     public string MacAddress { get; set; }
-    public ActionType Action { get; set; }
     public DateTime CreatedAt { get; set; }
     
 }
