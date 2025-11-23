cda87e98e4e8a0dc967617c2e6851231003074a5
Rlianny <revelianny10@gmail.com>
2025-11-23 12:26:18 -0500

    feat: Configure Dependency Injection

--- src/EasyPeasy_Login.Web/Program.cs
+++ src/EasyPeasy_Login.Web/Program.cs
@@ -3,6 +3,9 @@ using EasyPeasy_Login.Application.Services;
 using EasyPeasy_Login.Domain.Helper;
 using EasyPeasy_Login.Infrastructure.Data.Repositories;
 using EasyPeasy_Login.Infrastructure.Network.Configuration;
+using EasyPeasy_Login.Application.Services.SessionManagement;
+using EasyPeasy_Login.Web.Middleware;
+using EasyPeasy_Login.Domain.Interfaces;
 //using EasyPeasy_Login.Web.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,9 +19,14 @@ builder.Services.AddControllers();
 // Register presentation services
 
 builder.Services.AddScoped<INetworkConfigurationService, NetworkConfigurationService>();
-// builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
-// builder.Services.AddScoped<IUserRepository, UserRepository>();
-// builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+builder.Services.AddScoped<ISessionRepository, SessionRepository>();
+builder.Services.AddScoped<IDeviceRepository, DeviceRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<ISessionManagementService, SessionManagementService>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 var app = builder.Build();
 
@@ -36,7 +44,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 // customized Middleware
-// app.UseRequestRouter();
+app.UseMiddleware<RequestRouterMiddleware>();
 
 
 // Endpoints
